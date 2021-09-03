5bb8cbe290757ea47b33e50e3786e809c1c8cca0
Vivaan5311 <76786933+Vivaan5311@users.noreply.github.com>
2021-09-03 09:06:55 -0400

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -12,7 +12,13 @@ var firebaseConfig = {
     
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
-  
+    
+
+    user_name = localStorage.getItem("user_name");
+    document.getElementById("user_name").innerHTML = " Welcome " + user_name + " ! "
+
+
+
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
        Room_names = childKey;
       //Start code
@@ -36,8 +42,8 @@ window.location = "index.html"
 function addRoom(){
       room_name = document.getElementById("room_name").value
       localStorage.setItem("room_name", room_name)
-      firebase.database().ref("/").child(user_name).update({
-            Room_name : room_name
+      firebase.database().ref("/").child(room_name).update({
+            RoomName : room_name
 })
 
 }
