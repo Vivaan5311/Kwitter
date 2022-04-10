c17251d74b2c9d6c550a806fc1562e66a24c1bd0
Vivaan5311 <76786933+Vivaan5311@users.noreply.github.com>
2022-04-10 10:13:23 -0400

    Add files via upload

--- kwitter_page.js
+++ kwitter_page.js
@@ -45,16 +45,12 @@ function getData() { firebase.database().ref("/"+room_name).on('value', function
       like_button="<button class='btn btn-warning'  id="+firebase_message_id+" value="+like+" onclick='updateLike(this.id)'>"
       span_with_tag = "<span class='glyphicon glyphicon-thumbs-up'>Like :" +like+"</span></button></hr>"
       row = name_with_tag+message_with_tag+like_button+span_with_tag
-      document.getElementById("output").innerHTML+= row;})})}
-getData();
-
-function updateLike(msg)
-{
-button_id = msg;
-likes = document.getElementById(button_id).value
-updated_likes = number(likes) + 1
-firebase.database().ref(room_name).child(message_id).update({
-      like : updated_likes
-});
-
-}
+      document.getElementById("output").innerHTML+= row;})})}getData();
+      function updateLike(msg){
+            button_id = msg;
+            likes = document.getElementById(button_id).value;
+            updated_likes = number(likes) + 1
+            firebase.database().ref(room_name).child(message_id).update({
+                  like : updated_likes});}
+            
+            
